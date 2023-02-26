41f3a837f5725d2342bd969a6f7ad06c011f2535
Businesskomg <lawrencedouglas02@gmail.com>
2023-02-26 19:35:37 +0000

    bold

--- Censor extension/content.js
+++ Censor extension/content.js
@@ -1,1 +1,8 @@
-document.body.style.backgroundColor = 'yellow';
+// Select all the text nodes on the page
+const textNodes = document.evaluate('//text()', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+
+// Loop through each text node and set its font weight to "bold"
+for (let i = 0; i < textNodes.snapshotLength; i++) {
+  const textNode = textNodes.snapshotItem(i);
+  textNode.parentNode.style.fontWeight = 'bold';
+}
